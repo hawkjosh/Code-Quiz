6058dcda4581b66bf4e771e730756f474a300b09
Josh Hawk <hawkjosh@icloud.com>
2022-06-29 14:09:16 -0400

    Added skeleton structure of HTML elements in index.html, and also created questions content array in questions.js

---
+++ assets/javascript/questions.js
@@ -0,0 +1,58 @@
+// Created array of objects with question content as the object keys
+let questionsList = [
+  {
+    num: 1,
+    question: 'Which symbol is used to separate JavaScript statements?',
+    answer: 'Semicolon (;)',
+    choices: [
+      'Semicolon (;)',
+      'Colon (:)',
+      'Comma (,)',
+      'Underscore (_)'
+    ]
+  },
+  {
+    num: 2,
+    question: 'JavaScript arrays are written with ______.',
+    answer: 'Square Brackets []',
+    choices: [
+      'Curly Brackets {}',
+      'Square Brackets []',
+      'Parentheses ()',
+      'Double Quotes ""'
+    ]
+  },
+  {
+    num: 3,
+    question: 'Which JavaScript method is used to write on the browser console?',
+    answer: 'console.log()',
+    choices: [
+      'console.write()',
+      'console.output()',
+      'console.log()',
+      'console.print()'
+  ]
+  },
+  {
+    num: 4,
+    question: 'JavaScript code can be written ______.',
+    answer: 'externally in a .js file or directly in the .html file',
+    choices: [
+      'externally in a .js file',
+      'directly in the .html file',
+      'externally in a .css file',
+      'externally in a .js file or directly in the .html file'
+    ]
+  },
+  {
+    num: 5,
+    question: 'Which is the correct syntax to call an external JavaScript file in the HTML document?',
+    answer: '<script src="file_name.js"></script>',
+    choices: [
+      '<script src="file_name.js"></script>',
+      '<link rel="scriptsheet" href="file_name.js" />',
+      '<script link="file_name.js"></script>',
+      '<script href="file_name.js"></script>'
+    ]
+  }
+]
